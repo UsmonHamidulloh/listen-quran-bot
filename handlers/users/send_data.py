14eb985fb951e6e99f2756c06281d908e0f3b438
UsmonHamidulloh <usman.x.0221@gmail.com>
2022-02-07 15:07:19 +0500

    to'liq suraning audiosini tashlaydigan bo'ldi

--- handlers/users/send_data.py
+++ handlers/users/send_data.py
@@ -1,4 +1,5 @@
-from aiogram import types
+from aiogram import types, exceptions
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from utils.alQuranCloud import get_sura_name
 from utils.quranEnc import get_data
 from loader import dp
@@ -31,7 +32,55 @@ async def send_data(msg: types.Message):
         else:
             await msg.reply("<b>Маълумот топилмади.</b>")
     else:
-        text = ("<b>Бот ишлатишда хатоликка йўл қойдингиз, илтимос тўғри сўров юборинг.</b>",
-                "Агар ботни ишлатишга бироз тушунмаган бўлсангиз, /help ни босинг."
+        try:
+            try:
+
+                if send_audio(int(msg.text)):
+                    name = get_sura_name(f'{int(msg.text)}:1')
+                    await msg.answer_audio(
+                        send_audio(int(msg.text)),
+                        caption=f"<b>«{name}» сураси</b> | <b>Mishary Alafasy</b>"
+                    )
+                else:
+                    ans = (
+                        "<b>Қуръонда 114 сура мавжуд.</b>",
+                        "Илтимос, 1 дан 114 гача бўлган сон киритинг"
+                    )
+                    await msg.reply('\n\n'.join(ans))
+            except exceptions.WrongFileIdentifier:
+                ans = (
+                    "<b>Ботда хатолик юз берди, бу хатолик устида ҳозирда ишлаяпмиз</b>"
+                    "<a href='https://telegra.ph/file/895a32ea912dd1409e82f.png'> </a>",
+                    "Telegram-да bot орқали 50 МБ ҳажмдан кўп бўлган аудио жўнатса "
+                    "<a href='https://core.telegram.org/bots/api#sendaudio'>бўлмас экан</a>. "
+                    "Баъзи сураларнинг ҳажми 50 МБ-дан ошиб кетяпти, шу сабабдан хатолик юз беряпти. "
+                    "Яқин орада бу хатоликка барҳам берамиз (биизниллаҳ).",
+                    "<b>Ноқулайликлар учун узр сўраймиз..)</b>"
                 )
-        await msg.answer("\n\n".join(text))
+                await msg.answer('\n\n'.join(ans), reply_markup=InlineKeyboardMarkup(
+                    inline_keyboard=[
+                        [
+                            InlineKeyboardButton('Админга мурожаат қилиш', url='https://t.me/h_toshkandiy')
+                        ],
+                        [
+                            InlineKeyboardButton('Веб саҳифа', url='https://listen-quran.cf'),
+                            InlineKeyboardButton('Telegram', url='https://t.me/ListenQuran_uz')
+                        ]
+                    ],
+                ))
+        except ValueError:
+            text = ("<b>Бот ишлатишда хатоликка йўл қойдингиз, илтимос тўғри сўров юборинг</b>",
+                    "Агар ботни ишлатишга бироз тушунмаган бўлсангиз, /help ни босинг."
+                    )
+            await msg.reply("\n\n".join(text))
+
+
+def send_audio(num):
+    if num < 10:
+        return f'http://server8.mp3quran.net/afs/00{num}.mp3'
+    elif num < 100:
+        return f'http://server8.mp3quran.net/afs/0{num}.mp3'
+    elif num <= 114:
+        return f'http://server8.mp3quran.net/afs/{num}.mp3'
+    else:
+        return False
